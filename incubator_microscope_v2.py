d0c348510267ee6a792d3fb7f42594578c9a4bb2
unknown <samuellanzhou@gmail.com>
2019-07-23 14:50:47 -0400

    big fixes and framework for multiplate support

--- incubator_microscope_v2.py
+++ incubator_microscope_v2.py
@@ -8,12 +8,19 @@ import os
 import numpy as np
 import cv2
 
-plate_dict = {
+plate_96 = {
     "Home": [0,0,0],
     "A1": [5,5,0],
     "B1": [10,10,0],
     "C1": [5,5,-5]
     }
+
+plate_list = {"6": "plate_6",
+              "12": "plate_12",
+              "24": "plate_24",
+              "48": "plate_48",
+              "96": plate_96
+              }
 
 jog_dict = {
     "w": None,
@@ -25,7 +32,7 @@ jog_dict = {
     }
 [xMin, xMax] = [0.0, 30.0]
 [yMin, yMax] = [0.0, 30.0]
-[zMin, zMax] = [0.0, -15.0]
+[zMin, zMax] = [-15.0, 0.0]
 
 class Camera:
     def __init__(self): #initialize instance of Camera
@@ -40,10 +47,12 @@ class Camera:
         print("Camera initialized.")
 
     def save_image(self, grabResult):
+        now = datetime.datetime.now()
+        nowstr = now.strftime("%H%M%f-%m_%d_%y")
         print("Saving image...")
         ipo = pylon.ImagePersistenceOptions()
         ipo.SetQuality(100)
-        filename = "test_file"
+        filename = nowstr + ".jpeg"
         try:
             image = self.converter.Convert(grabResult)
             img = image.GetArray()
@@ -53,7 +62,6 @@ class Camera:
         except Exception as error:
             print("Error saving image:", error)
         return True
-        
 
     def acquire_image(self):
         print("Grabbing image...")
@@ -71,7 +79,7 @@ class Camera:
         self.camera.Close()
         return True
     
-    def night_cycle(self):
+    def night_cycle(self, well_num):
         #home machine
         #how many wells
         #how long
@@ -80,28 +88,39 @@ class Camera:
 class CNC:
     def __init__(self):
         self.axes = serial.Serial("COM4", baudrate = 115200, timeout = 1)
-        time.sleep(5)
         #self.LED = serial.Serial #initialize LED
+        time.sleep(2)
         print("Homing device...")
         #self.axes.write("$22 = 1\n".encode())
         self.axes.write("$x\n".encode())
         #self.axes.write("$h\n".encode())
+        print("CNC initialized.")
 
     #def home_device(self):
     #    self.axes.write("$h\n".encode())
     #    return True
 
+    def wellplate(self, plate_list):
+        while True:
+            print("Number of wells in well plate?")
+            well_num = input(">> ")
+            if num_wells in plate_list:
+                well_num = plate_list[well_num]
+                return well_num
+            print("Please enter a valid number (6, 12, 24, 48, 96).")
+        
+
     def well_move(self, plate, position):
-        well = input("Which well (ex. A1)?")
+        print("Which well (ex. A1)?")
         print("Type 'esc' to go back.")
-        print(">> ")
+        well = input(">> ")
         if well in plate and position == plate[well]:
             print("Already at", well)
         elif well in plate and position != plate[well]:
-            well_x = str(plate[well][0])
-            well_y = str(plate[well][1])
-            well_z = str(plate[well][2])
-            gcode_command = f"G0 X{well_x} Y{well_y} Z{well_z}\n"
+            x_move = str(plate[well][0] - position[0])
+            y_move = str(plate[well][1] - position[1])
+            z_move = str(plate[well][2] - position[2])
+            gcode_command = f"G0 X{x_move} Y{y_move} Z{z_move}\n"
             self.axes.write(gcode_command.encode())
             position = plate[well]
             print("Current position:", position)
@@ -112,54 +131,64 @@ class CNC:
             print("Invalid input. Please try again.")
         return position
     
-    def jog(self, jog_increment, jog_dict, position, xMin, xMax, yMin, yMax, zMin, zMax):
-        while True:
-            jog_input = input("Use the 'wasd' keys to jog the X and Y axes. Use the q and e keys to jog the Z axis.\n>> ")
-            print("Type 'esc' to exit.")
-            if jog_input in jog_dict:
-                x = round(self.position[0] + jog_dict[jog_input][0], 5)
-                y = round(self.position[1] + jog_dict[jog_input][1], 5)
-                z = round(self.position[2] + jog_dict[jog_input][2], 5)
-                if x < xMin or x > xMax or y < yMin or y > yMax or z < zMin or z > zMax:
-                    print("Beyond axis limit.")
+    def jog(self, jog_dict, position, xMin, xMax, yMin, yMax, zMin, zMax):
+        jog_increment = input("Enter a jog increment (mm): ")
+        try:
+            jog_increment = float(jog_increment)
+            if jog_increment > 0:
+                jog_dict["w"] = [0, jog_increment, 0]
+                jog_dict["a"] = [-jog_increment, 0, 0]
+                jog_dict["s"] = [0, -jog_increment, 0]
+                jog_dict["d"] = [jog_increment, 0, 0]
+                jog_dict["q"] = [0, 0, -jog_increment]
+                jog_dict["e"] = [0, 0, jog_increment]
+                while True:
+                    print("Use the 'wasd' keys to jog the X and Y axes. Use the q and e keys to jog the Z axis.\n>> ")
+                    print("Type 'esc' to exit. Type 'back' to change the jog increment.")
+                    jog_input = input(">> ")
+                    if jog_input in jog_dict:
+                        x = round(position[0] + jog_dict[jog_input][0], 5)
+                        y = round(position[1] + jog_dict[jog_input][1], 5)
+                        z = round(position[2] + jog_dict[jog_input][2], 5)
+                        if x < xMin or x > xMax or y < yMin or y > yMax or z < zMin or z > zMax:
+                            print("Beyond axis limit.")
+                        else:
+                            jog_command = f"G91 X{jog_dict[jog_input][0]} Y{jog_dict[jog_input][1]} Z{jog_dict[jog_input][2]}\n"
+                            self.axes.write(jog_command.encode())
+                            position = [x,y,z]
+                            print("Current position:", position)
+                    elif jog_input == "back":
+                        break
+                    elif jog_input == "esc":
+                        print("Current position:", position)
+                        return position
+                    else:
+                        print("Invalid input. Please try again.")
                 else:
-                    gcode_command = "G91 X{jog_dict[jog_input][0]} Y{jog_dict[jog_input][1]} Z{jog_dict[jog_input][2]}\n"
-                    self.axes.write(gcode_command.encode())
-                    position = [x,y,z]
-                    print("Current position:", self.position)
-            elif jog_input == "esc":
-                print("Current position:", self.position)
-                return position
-            else:
-                print("Invalid input. Please try again.")
+                    print("Input must be a number greater than zero")
+        except ValueError as error:
+            print(error)
+            print("Input must be number greater than zero.")
+            return position
+            
 
 def main():
-    camera = Camera()
+    camera = Camera()    
     machine = CNC()
     machine.position = [0,0,0]
+    plate_num = machine.wellplate(plate_list)
     while True:
         print(machine.position)
-        print("Enter a to move to a well. Enter b to jog the axes. Enter p to take a picture.")
+        print("Enter a to move to a well. Enter b to jog the axes. Enter p to take a picture. To change the well plate number, enter c.")
         main_input = input(">> ")
         if main_input == "a":
-            machine.position = machine.well_move(plate_dict, machine.position)
+            machine.position = machine.well_move(plate_num, machine.position)
         elif main_input == "b":
-            jog_increment = input("Enter a jog increment (mm): ")
-            try:
-                jog_increment = float(jog_increment)
-                if jog_increment > 0:
-                    jog_dict["w"] = [0, jog_increment, 0]
-                    jog_dict["a"] = [-jog_increment, 0, 0]
-                    jog_dict["s"] = [0, -jog_increment, 0]
-                    jog_dict["d"] = [jog_increment, 0, 0]
-                    jog_dict["q"] = [0, 0, -jog_increment]
-                    jog_dict["e"] = [0, 0, jog_increment]
-                    machine.position = machine.jog(jog_increment, jog_dict, machine.position, xMin, xMax, yMin, yMax, zMin, zMax)
-            except ValueError as error:
-                print(error)
-                print("Input must be number greater than zero.")
+            machine.position = machine.jog(jog_dict, machine.position, xMin, xMax, yMin, yMax, zMin, zMax)
         elif main_input == "p":
             camera.acquire_image()
+        elif main_input == "c":
+            plate_num = machine.wellplate(plate_list)
         else:
             print("Invalid input. Please try again.")
 
